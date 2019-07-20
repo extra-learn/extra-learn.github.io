62c0899d67e56deaaa792a39c2a19280f02c4e18
Konstantin Savchenko <killerkennyx41@gmail.com>
2019-07-20 21:05:53 +0300

    Extra Learn Home page(desktop)

---
+++ js/singUpForm.js
@@ -0,0 +1,28 @@
+$('select').styler({
+	selectSearch: true
+});
+
+const singUpForCourseForm = document.querySelector('.singUpForCourse');
+
+singUpForCourseForm.addEventListener('click', (event) => {
+  if (event.target === document.querySelector('.fieldNameBeforeClick')) {
+    event.target.classList.add('fieldNameAfterClick');
+    event.target.classList.remove('fieldNameBeforeClick');
+  } else if (event.target === document.querySelector('input') || event.target === document.querySelector('textarea')) {
+    event.target.previousElementSibling.classList.add('fieldNameAfterClick');
+    event.target.previousElementSibling.classList.remove('fieldNameBeforeClick');
+  }
+});
+
+let currentAndPreviousFields = [];
+
+const formQuestionsContainerField = document.querySelector('.formQuestionsContainer');
+
+formQuestionsContainerField.addEventListener('click', (event) => {
+  if (event.target === document.querySelector('textarea')) {
+    event.target.previousElementSibling.classList.add('fieldNameAfterClick');
+    event.target.previousElementSibling.classList.remove('fieldNameBeforeClick');
+    event.target.style.height = '51px';
+    event.target.style.lineHeight = '17px';
+  }
+});
